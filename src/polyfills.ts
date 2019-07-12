3d42e27d83d23a99d8df6b11ad0b1fb9083f8ca6
Nastaran Heydari <nh.1374@yahoo.com>
2019-07-12 21:56:52 +0430

    ---

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 // import 'core-js/es6/reflect';
-import 'core-js/es/reflect';
+import 'core-js/features/reflect';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
